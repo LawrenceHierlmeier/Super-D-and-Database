aa09a3992f0016f123ed2ef3db82f27217536ecf
= <lawrencehierlmeier0@gmail.com>
2022-04-18 14:19:34 -0500

    Added Navigation Bar and page templates

--- application/app.py
+++ application/app.py
@@ -1,21 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_navigation import Navigation
+
 import database
 
 app = Flask(__name__)
+nav = Navigation(app)
 #dungeon_database.create_class_table()
+
+# Initializing Navigation
+nav.Bar('top', [
+    nav.Item('Home', 'home'),
+    nav.Item('Characters', 'character_list')
+])
 
 
 @app.route('/')
 def home():
-    return 'Hello World'
+    return render_template('home.html')
 
 
 @app.route('/characters')
 def character_list():
-    chars = dungeon_database.list_characters()
-    char_list = ' '.join(str(e) for e in chars)
+    chars = database.list_characters()
 
-    return char_list
+    #char_list = ' '.join(str(e) for e in chars)
+
+    return render_template('character_list.html', chars = chars)
+    #return chars
 
 
 if __name__ == '__main__':
