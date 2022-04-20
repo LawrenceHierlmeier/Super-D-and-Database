03f88ba0184dcba16bd05eb27d0cb79a83dca890
Pierce Dreiling <99362218+Pierce-01@users.noreply.github.com>
2022-04-20 15:17:25 -0500

    added relation tables, fixed errors

--- creation.sql
+++ creation.sql
@@ -11,17 +11,13 @@ CREATE TABLE CHARACTER (
     FEAT_NAME    CHAR(30),
     PRIMARY KEY (NAME),
     FOREIGN KEY (RACE_NAME) REFERENCES RACE(NAME)
-        ON UPDATE CASCADE,    /*if a race's name is altered, update it*/
-    FOREIGN KEY (CLASS_NAME) REFERENCES CLASS(NAME)
-        ON UPDATE CASCADE,    /*if a class's name is altered, update it*/
-    FOREIGN KEY (FEAT_NAME) REFERENCES FEATS(NAME)
-        ON UPDATE CASCADE    /*if a feat's name is altered, update it*/
+        ON UPDATE CASCADE    /*if a race's name is altered, update all character tuples with that race name*/
     );
     
 CREATE TABLE INVENTORY (
     ITEM     CHAR(30),
     ITEM_WEIGHT    INT    CHECK(ITEM_WEIGHT>0),
-    CHARACTER_NAME     CHAR(30),
+    CHARACTER_NAME     CHAR(30)    NOT NULL,
     FOREIGN KEY (CHARACTER_NAME) REFERENCES CHARACTER(NAME)
         ON DELETE CASCADE    /*deleting character also deletes inventory*/
     );
@@ -33,6 +29,8 @@ CREATE TABLE CAMPAIGN (
     NUM_PLAYERS    INT    NOT NULL    CHECK(NUM_PLAYERS>=0),
     CHARACTER_NAME    CHAR(30)    NOT NULL,
     PRIMARY KEY (NAME)
+    FOREIGN KEY (CHARACTER_NAME) REFERENCES CHARACTER(NAME)
+        ON UPDATE CASCADE /*updating a character's name updates all campaign tuples with that character*/
     );
     
 CREATE TABLE CLASS (
@@ -46,8 +44,8 @@ CREATE TABLE CLASS (
     CHARACTER_NAME    CHAR(30),
     PRIMARY KEY (NAME),
     FOREIGN KEY (CHARACTER_NAME) REFERENCES CHARACTER(NAME)
-        ON DELETE CASCADE    /*deleting a character deletes all tuples with that character*/
-        ON UPDATE CASCADE    /*updating a character's name updates all tuples with that character*/
+        ON DELETE CASCADE    /*deleting a character deletes all class tuples with that character*/
+        ON UPDATE CASCADE    /*updating a character's name updates all class tuples with that character*/
     );
 
 CREATE TABLE FEATS (
@@ -57,8 +55,8 @@ CREATE TABLE FEATS (
     CHARACTER_NAME    CHAR(30),
     PRIMARY KEY (NAME),
     FOREIGN KEY (CHARACTER_NAME) REFERENCES CHARACTER(NAME)
-        ON DELETE CASCADE    /* deleting a character deletes all tuples with that character*/
-        ON UPDATE CASCADE    /* updating a character's name updates all tuples with that character*/
+        ON DELETE CASCADE    /* deleting a character deletes all feats tuples with that character*/
+        ON UPDATE CASCADE    /* updating a character's name updates all feats tuples with that character*/
     );
     
 CREATE TABLE RACE (
@@ -79,6 +77,28 @@ CREATE TABLE SUB_RACE (
     RACIAL_FEATS    CHAR(100),
     PRIMARY_RACE_NAME    CHAR(30)    NOT NULL,
     FOREIGN KEY (PRIMARY_RACE_NAME) REFERENCES RACE(NAME)
-        ON DELETE CASCADE    /*deleting primary race deletes sub race*/
-        ON UPDATE CASCADE    /* updating primary race name updates all tuples with that name*/
+        ON DELETE CASCADE    /*deleting primary race deletes all sub race tuples with that primary race*/
+        ON UPDATE CASCADE    /* updating primary race name updates all sub race tuples with that primary race name*/
+    );
+    
+CREATE TABLE HAS ( /*HAS relation between CHARACTER and CLASS*/
+    CLASS_NAME    CHAR(30)    NOT NULL,
+    CHARACTER_NAME    CHAR(30)    NOT NULL,
+    FOREIGN KEY (CLASS_NAME) REFERENCES CLASS(NAME)
+        ON DELETE CASCADE    /*if class is deleted, delete all HAS tuples with that class name*/
+        ON UPDATE CASCADE,    /*if class name is updated, update all HAS tuples with that class name*/
+    FOREIGN KEY (CHARACTER_NAME) REFERENCES CHARACTER(NAME)
+        ON DELETE CASCADE    /*if character is deleted, delete all HAS tuples with that character name*/
+        ON UPDATE CASCADE    /*if character name is updated, update all HAS tuples with that character name*/
+    );
+    
+CREATE TABLE POSSESSES ( /*POSSESSES relation between CHARACTER and FEATS*/
+    CHARACTER_NAME    CHAR(30)    NOT NULL,
+    FEAT_NAME    CHAR(30)    NOT NULL,
+    FOREIGN KEY (CHARACTER_NAME) REFERENCES CHARACTER(NAME)
+        ON DELETE CASCADE    /*if character is deleted, delete all POSSESSES tuples with that character name*/
+        ON UPDATE CASCADE,    /*if character name is updated, update all POSSESSES tuples with that character name*/
+    FOREIGN KEY (FEAT_NAME) REFERENCES FEAT(NAME)
+        ON DELETE CASCADE    /*if feat is deleted, delete all POSSESSES tuples with that feat name*/
+        ON UPDATE CASCADE    /*if feat name is updated, update all POSSESSES tuples with that feat name*/
     );
