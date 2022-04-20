0b4a654b2e6e3f7eaeb88aecadf2c47d36d2d0f7
carmvg <55164499+carmvg@users.noreply.github.com>
2022-04-20 14:57:52 -0500

    Added Delete_Character function

--- application/database.py
+++ application/database.py
@@ -12,11 +12,13 @@ def list_characters():
         return chars
 
 def delete_character(name):
-    with DatabaseConnection('dungeon_data.db') as connection:
+    with DatabaseConnection('CS2300Proj.db') as connection:
         cursor = connection.cursor()
-
-        cursor.execute(f"SELECT * FROM Character AS C WHERE C.name = name")
-
+        
+        delete_query = "DELETE FROM CHARACTER WHERE id = ?"
+        cursor.execute(delete_query, name)
+        cursor.close()
+  
 def add_feats():
     pass
 
@@ -38,7 +40,11 @@ def show_character_inventory():
 def add_character_to_campaign():
     pass
 
-def add_campaign():
+def add_campaign(name, region, num_npc, num_character):
+    '''
+    with DatabaseConnection('CS2300Proj.db') as connection:
+        cursor = connection.cursor()
+    '''
     pass
 
 def add_character():
