26076ac71c519765b3cd2e62afdab7c36fae0ade
carmvg <55164499+carmvg@users.noreply.github.com>
2022-04-20 14:22:51 -0500

    Added functions

--- application/database.py
+++ application/database.py
@@ -10,3 +10,36 @@ def list_characters():
         chars = [{'name': row[0], 'race': row[1], 'class': row[2]} for row in cursor.fetchall()]
 
         return chars
+
+def delete_character(name):
+    with DatabaseConnection('dungeon_data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute(f"SELECT * FROM Character AS C WHERE C.name = name")
+
+def add_feats():
+    pass
+
+def modify_character():
+    pass
+
+def delete_campaign():
+    pass
+
+def remove_character_from_campaign():
+    pass
+
+def add_item_to_inventory():
+    pass
+
+def show_character_inventory():
+    pass
+
+def add_character_to_campaign():
+    pass
+
+def add_campaign():
+    pass
+
+def add_character():
+    pass
